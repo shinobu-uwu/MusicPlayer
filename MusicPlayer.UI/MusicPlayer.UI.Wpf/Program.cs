4df8ae1c466d15f49bf9910e8b139b678b36afef
Matheus Filipe dos Santos Reinert <matheus.reinert@protonmail.com>
2021-07-08 20:41:24 -0300

    Very initial GUI implementation

---
+++ MusicPlayer.UI/MusicPlayer.UI.Wpf/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Eto.Forms;
+
+namespace MusicPlayer.UI.Wpf
+{
+    class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            new Application(Eto.Platforms.Wpf).Run(new MainForm());
+        }
+    }
+}
